f8726cb83241d037070447c56a9facf888274ff9
Zoltan Podlovics <zpodlovics@users.noreply.github.com>
2014-08-22 15:36:14 +0200

    Fix path

--- tests/FsHlvm.Core.Tests/run.sh
+++ tests/FsHlvm.Core.Tests/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mono --runtime=v4.0 --jitmap ../../packages/NUnit.Runners.2.6.3/tools/nunit-console.exe bin/*/FsHlvm.Tests.dll $1
+mono --runtime=v4.0 --jitmap ../../packages/NUnit.Runners.2.6.3/tools/nunit-console.exe bin/Release/FsHlvm.Core.Tests.dll $1
